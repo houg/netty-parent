30158d07a339a3a4abef0a50528a16324c851f24
曹莲 <@LAPTOP-4PMRDHTD>
2021-05-01 23:02:15 +0800

    new project

---
+++ netty-client/src/main/java/com/ganghou/netty/client/App.java
@@ -0,0 +1,13 @@
+package com.ganghou.netty.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ netty-route-common/src/main/java/com/ganghou/netty/route/common/App.java
@@ -0,0 +1,13 @@
+package com.ganghou.netty.route.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
